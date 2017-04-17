4be9bcc95645d034c6ba9a3dc9aff17c59f2fd9b
Jarrell H Walker <jwalker-burnam@mail.csuchico.edu>
2017-04-17 13:44:28 -0700

    Updates to styling and animations
    added some effects and made some styling chages.
    -fixed a few bugs that caused some cross browser issues.

--- Portfolio 3.0/src/js/main.js
+++ Portfolio 3.0/src/js/main.js
@@ -1,6 +1,30 @@
-
-$(document).ready(function(){
+$(document).ready(function () {
     $('.toggle a').click(function () {
         $('.menu').toggleClass('active');
     })
+});
+window.sr = ScrollReveal();
+sr.reveal('.read', {
+    duration: 2000,
+    origin: 'bottom'
+});
+sr.reveal('.icons', {
+    duration: 2000,
+    origin: 'left',
+    delay: 1000
+});
+//Scroll function to section
+$(function () {
+    $('a[href*="#"]:not([href="#"])').click(function () {
+        if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
+            var target = $(this.hash);
+            target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+            if (target.length) {
+                $('html, body').animate({
+                    scrollTop: target.offset().top
+                }, 1000);
+                return false;
+            }
+        }
+    });
 });
